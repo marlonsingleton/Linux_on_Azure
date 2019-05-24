b1a420f425e3c12f6f7d72e3045fafc806d25800
Marlon Singleton <marlonosingleton@yahoo.com>
2019-05-25 02:14:28 +0300

    added comments

--- setup_env_azurecli.sh
+++ setup_env_azurecli.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# create resourse group
 az group create --name myResourceGroupAutomate --location eastus
 
+# create linux vm and include cloud-init for linux config
 az vm create \
     --resource-group myResourceGroupAutomate \
     --name myVM \
@@ -9,5 +11,6 @@ az vm create \
     --admin-username azureuser \
     --generate-ssh-keys \
     --custom-data cloud-init.txt
-    
-    az vm open-port --port 80 --resource-group myResourceGroupAutomate --name myVM
+
+# open port 80
+az vm open-port --port 80 --resource-group myResourceGroupAutomate --name myVM
